176d9dec11410a87104642af8cb47aae6ae63f61
Kartikey Tewari <kartikeya30@gmail.com>
2020-09-13 00:11:51 +0530

    added user login

--- server.js
+++ server.js
@@ -52,6 +52,18 @@ app.get("/login", (req,res) => {
     res.render("login");
 });
 
+app.post("/login", (req,res) =>{
+    User.findOne({ email: req.body.email}, (err,user) => {
+        if (err || !user || req.body.password !== user.password){
+            return res.render ("login", {
+                error: "Incorrect email or password"
+            });
+        }
+
+        res.redirect("/dashboard");
+    });
+});
+
 app.get("/dashboard", (req,res) => {
     res.render("dashboard");
 });
